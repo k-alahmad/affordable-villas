f75ba2daf38707b987bbd66a3a2e36822f4586d5
moazabdulhalim@gmail.com <moazabdulhalim@gmail.com>
2023-12-21 19:08:41 +0300

    fixes

--- src/pages/home/components/RegisterForm.jsx
+++ src/pages/home/components/RegisterForm.jsx
@@ -8,150 +8,202 @@ import { Gender } from "../../../constants";
 import CustomInput from "../../../components/Forms/CustomInput";
 import useForm from "../../../hooks/useForm";
 import { useDispatch } from "react-redux";
-import { showMessage } from "../../../redux/messageAction.slice";
 import { projectsData } from "../../../data/projectsData";
+import emailjs from "@emailjs/browser";
+import { selectListRegisterState } from "../../../redux/modal.slice";
+import { useSelector } from "react-redux";
 const defaultFormState = {
-	Email: "",
-	FullName: "",
-	Gender: "Male",
-	PhoneNo: "",
-	Project: projectsData[0].ProjectName.en,
+  Email: "",
+  FullName: "",
+  Gender: "Male",
+  PhoneNo: "",
+  Project: projectsData[0].ProjectName.en,
 };
 const RegisterForm = () => {
-	const { t, i18n } = useTranslation();
-	const dispatch = useDispatch();
-	const {
-		disabled,
-		setErrors,
-		errors,
-		setValues,
-		values,
-		handleChange,
-		handleSubmit,
-	} = useForm(submit, defaultFormState);
-	const isLoading = false;
-	function submit(e) {
-		setValues(defaultFormState);
-	}
-	return (
-		<div id='register' className='w-[75vw] max-w-[700px] my-12'>
-			<p className='text-secondary font-semibold text-4xl mb-4 flex justify-center items-center'>
-				For more information
-			</p>
-			<form className='flex flex-col justify-between items-stretch h-full w-full space-y-4'>
-				<CustomInput
-					icon={<MdPerson className='text-white text-med' />}
-					placeholder={t("formFullName")}
-					type='text'
-					name='FullName'
-					id='FullName'
-					value={values.FullName}
-					onChange={handleChange}
-					error={Boolean(errors?.FullName)}
-				/>
+  const { t, i18n } = useTranslation();
+  const dispatch = useDispatch();
 
-				<CustomInput
-					icon={<MdMail className='text-white text-med' />}
-					placeholder={t("formEmail")}
-					type='email'
-					name='Email'
-					id='Email'
-					value={values.Email}
-					onChange={handleChange}
-					error={Boolean(errors?.Email)}
-				/>
+  const listRegister = useSelector(selectListRegisterState);
+  const {
+    disabled,
+    setErrors,
+    errors,
+    setValues,
+    values,
+    handleChange,
+    handleSubmit,
+  } = useForm(submit, defaultFormState);
+  const isLoading = false;
 
-				<PhoneInput
-					country={"ae"}
-					placeholder={t("formPhoneNumber")}
-					enableSearch={true}
-					inputProps={{
-						name: "phone",
-						id: "phone",
-						required: true,
-					}}
-					onChange={(phone) => {
-						if (phone.length < 10) {
-							setErrors({
-								...errors,
-								PhoneNo: "Phone Number is atleast 10 digits",
-							});
-						} else {
-							let newObj = omit(errors, "PhoneNo");
-							setErrors(newObj);
-						}
-						setValues({ ...values, PhoneNo: phone });
-					}}
-					value={values.PhoneNo}
-					containerStyle={{
-						outline: "none",
-						outlineOffset: "0px",
-						boxShadow: "none",
-					}}
-					containerClass={`${
-						Boolean(errors.PhoneNo)
-							? "!border-[1px] border-red-500"
-							: "!border-b-[1px] border-white"
-					} px-1 flex bg-white/20 rounded-md !outline-none`}
-					inputClass={`!bg-transparent !text-white !w-full !py-3 !text-lg !h-full !border-none ${
-						i18n.language == "en" ? "px-0" : "mx-10"
-					} !outline-none`}
-					buttonClass={`!border-none !outline-none !text-lg `}
-					buttonStyle={{
-						direction: "ltr",
-						outline: "none",
-						outlineOffset: "0px",
-						boxShadow: "none",
-					}}
-					dropdownClass='!bg-primary/70 !backdrop-blur-[21px] !text-third '
-					searchClass='!bg-primary/70 !backdrop-blur-[21px] !text-secondary'
-					inputStyle={{
-						direction: "ltr",
-						outline: "none",
-						outlineOffset: "0px",
-						boxShadow: "none",
-					}}
-				/>
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_5wdnu6j",
+        "template_slqqcpm",
+        form.current,
+        "sxh5TJan60LQqD6Sw"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  async function submit(e) {
+    try {
+      let sameEmail = listRegister.find((element) => {
+        return element.email == form.current.email.value;
+      });
+      let samePhone = listRegister.find((element) => {
+        return element.phone == form.current.phone.value;
+      });
+      if (!sameEmail && !samePhone) {
+        const response = await fetch(import.meta.env.VITE_ZAPPIER_URL, {
+          method: "POST",
+          body: values,
+          "Content-Type": "multipart/form-data",
+        });
+        sendEmail(e);
 
-				<div className='space-y-1'>
-					<p className='text-tiny text-white'>{t("Project")} </p>
+        dispatch(
+          register({
+            email: values.Email,
+            phoneNo: values.PhoneNo,
+          })
+        );
 
-					<div className='flex justify-center items-center border-[1px] rounded-md p-1 gap-x-2'>
-						{projectsData.map((item, index) => {
-							return (
-								<React.Fragment key={index}>
-									<div
-										className={`py-3 rounded-md text-tiny w-full flex justify-center items-center cursor-pointer transition-all duration-300 tracking-tighter ${
-											values.Project == item.ProjectName.en
-												? "bg-secondary text-primary"
-												: "bg-transparent text-white"
-										}`}
-										onClick={() =>
-											setValues({ ...values, Project: item.ProjectName.en })
-										}
-									>
-										{item.ProjectName[i18n.language]}
-									</div>
-									{index !== projectsData.length - 1 && (
-										<div className='h-10 w-1 bg-white/50' />
-									)}
-								</React.Fragment>
-							);
-						})}
-					</div>
-				</div>
-				<button
-					className={`bg-secondary text-primary font-semibold text-smaller md:text-small w-full py-2  disabled:bg-none disabled:bg-gray-500 disabled:text-white rounded-md ${
-						isLoading && "animate-pulse"
-					} `}
-					onClick={handleSubmit}
-					disabled={disabled}
-				>
-					{isLoading ? t("sending") : t("send")}
-				</button>
-			</form>
-		</div>
-	);
+        navigate("/thankyou");
+      } else {
+        alert("We Already Have a registration with this data");
+      }
+    } catch (error) {
+      console.error("Error here:", error);
+    }
+    setValues(defaultFormState);
+  }
+  return (
+    <div id="register" className="w-[75vw] max-w-[700px] my-12">
+      <p className="text-secondary font-semibold text-4xl mb-4 flex justify-center items-center">
+        For more information
+      </p>
+      <form className="flex flex-col justify-between items-stretch h-full w-full space-y-4">
+        <CustomInput
+          icon={<MdPerson className="text-white text-med" />}
+          placeholder={t("formFullName")}
+          type="text"
+          name="FullName"
+          id="FullName"
+          value={values.FullName}
+          onChange={handleChange}
+          error={Boolean(errors?.FullName)}
+        />
+
+        <CustomInput
+          icon={<MdMail className="text-white text-med" />}
+          placeholder={t("formEmail")}
+          type="email"
+          name="Email"
+          id="Email"
+          value={values.Email}
+          onChange={handleChange}
+          error={Boolean(errors?.Email)}
+        />
+
+        <PhoneInput
+          country={"ae"}
+          placeholder={t("formPhoneNumber")}
+          enableSearch={true}
+          inputProps={{
+            name: "phone",
+            id: "phone",
+            required: true,
+          }}
+          onChange={(phone) => {
+            if (phone.length < 10) {
+              setErrors({
+                ...errors,
+                PhoneNo: "Phone Number is atleast 10 digits",
+              });
+            } else {
+              let newObj = omit(errors, "PhoneNo");
+              setErrors(newObj);
+            }
+            setValues({ ...values, PhoneNo: phone });
+          }}
+          value={values.PhoneNo}
+          containerStyle={{
+            outline: "none",
+            outlineOffset: "0px",
+            boxShadow: "none",
+          }}
+          containerClass={`${
+            Boolean(errors.PhoneNo)
+              ? "!border-[1px] border-red-500"
+              : "!border-b-[1px] border-white"
+          } px-1 flex bg-white/20 rounded-md !outline-none`}
+          inputClass={`!bg-transparent !text-white !w-full !py-3 !text-lg !h-full !border-none ${
+            i18n.language == "en" ? "px-0" : "mx-10"
+          } !outline-none`}
+          buttonClass={`!border-none !outline-none !text-lg `}
+          buttonStyle={{
+            direction: "ltr",
+            outline: "none",
+            outlineOffset: "0px",
+            boxShadow: "none",
+          }}
+          dropdownClass="!bg-primary/70 !backdrop-blur-[21px] !text-third "
+          searchClass="!bg-primary/70 !backdrop-blur-[21px] !text-secondary"
+          inputStyle={{
+            direction: "ltr",
+            outline: "none",
+            outlineOffset: "0px",
+            boxShadow: "none",
+          }}
+        />
+
+        <div className="space-y-1">
+          <p className="text-tiny text-white">{t("Project")} </p>
+
+          <div className="flex justify-center items-center border-[1px] rounded-md p-1 gap-x-2">
+            {projectsData.map((item, index) => {
+              return (
+                <React.Fragment key={index}>
+                  <div
+                    className={`py-3 rounded-md text-tiny w-full flex justify-center items-center cursor-pointer transition-all duration-300 tracking-tighter ${
+                      values.Project == item.ProjectName.en
+                        ? "bg-secondary text-primary"
+                        : "bg-transparent text-white"
+                    }`}
+                    onClick={() =>
+                      setValues({ ...values, Project: item.ProjectName.en })
+                    }
+                  >
+                    {item.ProjectName[i18n.language]}
+                  </div>
+                  {index !== projectsData.length - 1 && (
+                    <div className="h-10 w-1 bg-white/50" />
+                  )}
+                </React.Fragment>
+              );
+            })}
+          </div>
+        </div>
+        <button
+          className={`bg-secondary text-primary font-semibold text-smaller md:text-small w-full py-2  disabled:bg-none disabled:bg-gray-500 disabled:text-white rounded-md ${
+            isLoading && "animate-pulse"
+          } `}
+          onClick={handleSubmit}
+          disabled={disabled}
+        >
+          {isLoading ? t("sending") : t("send")}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterForm;
--- src/redux/modal.slice.js
+++ src/redux/modal.slice.js
@@ -7,6 +7,8 @@ const initialState = {
   galleryData: null,
   settingsModal: false,
   settingsData: null,
+  registered: JSON.parse(localStorage.getItem("Registered")) ?? false,
+  listRegistered: JSON.parse(localStorage.getItem("listRegistered")) ?? [],
 };
 const modalSlice = createSlice({
   name: "modal",
@@ -36,6 +38,20 @@ const modalSlice = createSlice({
       state.settingsModal = false;
       state.settingsData = null;
     },
+    register: (state, action) => {
+      let newRegister = {
+        email: action.payload.email,
+        phoneNo: action.payload.phoneNo,
+      };
+      state.listRegistered.push(newRegister);
+      localStorage.setItem(
+        "listRegistered",
+        JSON.stringify(state.listRegistered)
+      );
+      state.registered = true;
+      localStorage.setItem("Registered", true);
+      state.download = false;
+    },
   },
 });
 
@@ -46,6 +62,7 @@ export const {
   hideGalleryModal,
   showSettingsModal,
   hideSettingsModal,
+  register,
 } = modalSlice.actions;
 export const selectModal = (state) => state.modal;
 export const selectState = (state) => state.modal.open;
@@ -54,5 +71,7 @@ export const selectSettingsModalState = (state) => state.modal.settingsModal;
 export const selectModalData = (state) => state.modal.data;
 export const selectGalleryModalData = (state) => state.modal.galleryData;
 export const selectSettingsModalData = (state) => state.modal.settingsData;
+export const selectRegisterState = (state) => state.modal.registered;
+export const selectListRegisterState = (state) => state.modal.listRegistered;
 
 export default modalSlice.reducer;
