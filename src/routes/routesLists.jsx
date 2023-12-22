7f02a7ea011c4b3806b65c00b1afd3eaa85f8de3
moazabdulhalim@gmail.com <moazabdulhalim@gmail.com>
2023-12-22 18:52:14 +0300

    added thank you in route

--- src/routes/routesLists.jsx
+++ src/routes/routesLists.jsx
@@ -1,10 +1,15 @@
 import { lazy, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import Home from "../pages/home";
+const thankYou = lazy(() => import("../pages/thankYou/index.jsx"));
 
 export const publicRoutes = [
   { path: "/park-greens", element: Home },
   { path: "/reportage-village", element: Home },
+  {
+    path: "/thankyou",
+    element: thankYou,
+  },
   {
     path: "/privacy-policy",
     element: lazy(() => import("../pages/privacy/index.jsx")),
